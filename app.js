df260f30913af3c2056cd7aadc458c7b47eea263
Sajani <22cis0309@ms.sab.ac.lk>
2026-09-11 11:06:56 +0530

    Implement game feedback with SweetAlert and add assets for win animations

--- app.js
+++ app.js
@@ -2,33 +2,30 @@
 const randomNumber = Math.floor(Math.random() * 10) + 1;
 console.log(randomNumber);
 
-Swal.fire({
-    title: "Submit your GitHub username",
-    input: "text",
-    inputAttributes: { autocapitalize: "off" },
-    showCancelButton: true,
-    confirmButtonText: "Look up",
-    showLoaderOnConfirm: true,
-    preConfirm: async (login) => {
-        try {
-            const githubUrl = `
-        https://api.github.com/users/${login}
-      `;
-            const response = await fetch(githubUrl);
-            if (!response.ok) return Swal.showValidationMessage(`
-          ${JSON.stringify(await response.json())}
-        `);
-            return response.json();
-        } catch (error) {
-            Swal.showValidationMessage(`
-        Request failed: ${error}
-      `);
-        }
-    },
-    allowOutsideClick: () => !Swal.isLoading()
-}).then((result) => {
-    if (result.isConfirmed) Swal.fire({
-        title: `${result.value.login}'s avatar`,
-        imageUrl: result.value.avatar_url
-    });
-});
+function checkGuess() {
+    let userGuess = document.getElementById("guessInput").value;
+    if (userGuess == randomNumber) {
+        Swal.fire({
+            title: "Congratulations!",
+            width: 600,
+            padding: "3em",
+            color: "#716add",
+            background: "#fff url(assets/backgroundimage.jpg)",
+            backdrop: `
+    rgba(0,0,123,0.4)
+    url("assets/win gif.gif")
+    left top
+    no-repeat
+  `
+        });
+    }
+    else {
+        Swal.fire({
+            title: "Try Again!",
+            text: "Your guess is incorrect. Please try again.",
+            icon: "error",
+            confirmButtonText: "OK"
+        });
+    }
+
+}
